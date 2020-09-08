a230690dadbe8c34b2cd8ac334bfa633b2e4b951
serratedstone <serratedstone.16@gmail.com>
2020-09-08 18:37:38 -0500

    - Cambios de los nombres de las etiquetas y valores del archivo strings.xml: idioma original: español, idioma actual: ingles. Para evitar los Typos. - Apartir de esto se corrigieron sus referencias en los archivos Layout correspondientes.

--- app/src/main/java/com/example/perceptordeinseguridad/TypeFragment.java
+++ app/src/main/java/com/example/perceptordeinseguridad/TypeFragment.java
@@ -19,13 +19,7 @@ import android.widget.ImageButton;
 public class TypeFragment extends Fragment implements View.OnClickListener {
 
     private ImageButton close;
-    private Button unintentionalViolence;
     private Button druggedDrunkPeople;
-    private Button pedestrianHazard;
-    private Button highPollution;
-    private Button healtHazard;
-    private Button humanDanger;
-    private Button localDanger;
     private Button harassment;
     private Button vandalism;
     private Button assault;
@@ -35,26 +29,12 @@ public class TypeFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedState) {
         View view = inflater.inflate(R.layout.fragment_type, container, false);
-        unintentionalViolence = view.findViewById(R.id.btn_unintentionalViolene);
         druggedDrunkPeople = view.findViewById(R.id.btn_druggedDrunkPeople);
-        pedestrianHazard = view.findViewById(R.id.btn_pedestrianHazard);
-        highPollution = view.findViewById(R.id.btn_highPollution);
         close = view.findViewById(R.id.btn_closeTypeOfDanger);
-        humanDanger = view.findViewById(R.id.btn_humanDanger);
-        localDanger = view.findViewById(R.id.btn_localDanger);
-        healtHazard = view.findViewById(R.id.btn_healtHazard);
         harassment = view.findViewById(R.id.btn_harassment);
         vandalism = view.findViewById(R.id.btn_vandalism);
         assault = view.findViewById(R.id.btn_asault);
         skip = view.findViewById(R.id.btn_skip2);
-        unintentionalViolence.setVisibility(View.GONE);
-        druggedDrunkPeople.setVisibility(View.GONE);
-        pedestrianHazard.setVisibility(View.GONE);
-        highPollution.setVisibility(View.GONE);
-        healtHazard.setVisibility(View.GONE);
-        harassment.setVisibility(View.GONE);
-        vandalism.setVisibility(View.GONE);
-        assault.setVisibility(View.GONE);
         args = new Bundle();
         args.putString("latitude2", requireArguments().getString("latitude"));
         args.putString("longitude2", requireArguments().getString("longitude"));
@@ -68,27 +48,8 @@ public class TypeFragment extends Fragment implements View.OnClickListener {
         super.onViewCreated(view, savedInstanceState);
         close.setOnClickListener(v ->
                 Navigation.findNavController(v).navigate(R.id.typeFrag_to_startLocFrag));
-        humanDanger.setOnClickListener(v -> {
-            humanDanger.setVisibility(View.GONE);
-            localDanger.setVisibility(View.GONE);
-            unintentionalViolence.setVisibility(View.VISIBLE);
-            druggedDrunkPeople.setVisibility(View.VISIBLE);
-            harassment.setVisibility(View.VISIBLE);
-            vandalism.setVisibility(View.VISIBLE);
-            assault.setVisibility(View.VISIBLE);
-        });
-        localDanger.setOnClickListener(v -> {
-            humanDanger.setVisibility(View.GONE);
-            localDanger.setVisibility(View.GONE);
-            pedestrianHazard.setVisibility(View.VISIBLE);
-            highPollution.setVisibility(View.VISIBLE);
-            healtHazard.setVisibility(View.VISIBLE);
-        });
-        unintentionalViolence.setOnClickListener(this);
+
         druggedDrunkPeople.setOnClickListener(this);
-        pedestrianHazard.setOnClickListener(this);
-        highPollution.setOnClickListener(this);
-        healtHazard.setOnClickListener(this);
         harassment.setOnClickListener(this);
         vandalism.setOnClickListener(this);
         assault.setOnClickListener(this);
@@ -97,24 +58,16 @@ public class TypeFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.btn_unintentionalViolene)
-            args.putString("type","violencia no intencional");
         if(v.getId() == R.id.btn_druggedDrunkPeople)
-            args.putString("type","alcoholizados/drogados");
-        if(v.getId() == R.id.btn_highPollution)
-            args.putString("type","alta contaminacion");
-        if(v.getId() == R.id.btn_healtHazard)
-            args.putString("type","peligro sanitario");
-        if(v.getId() == R.id.btn_pedestrianHazard)
-            args.putString("type","peligro peatonal");
+            args.putString("type","1");
         if(v.getId() == R.id.btn_vandalism)
-            args.putString("type","vandalismo");
+            args.putString("type","2");
         if(v.getId() == R.id.btn_asault)
-            args.putString("type", "asalto");
+            args.putString("type", "3");
         if(v.getId() == R.id.btn_harassment)
-            args.putString("type","acoso");
+            args.putString("type","4");
         if(v.getId() == R.id.btn_skip2)
-            args.putString("type", "desconocido");
+            args.putString("type", "5");
         Navigation.findNavController(v).navigate(R.id.typeFrag_to_sendingFrag, args);
     }
 }
